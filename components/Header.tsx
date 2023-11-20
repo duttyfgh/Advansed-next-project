eccc13890144013795851591ae4b32e0a2730c7b
dutyfgh <kastrula09@gmail.com>
2023-11-20 17:38:02 +0200

    add: dropdown in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,21 @@
 import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import DarkModeToggle from './DarkModeToggle/DarkModeToggle'
 import cls from './Header.module.css'
 
 const Header = () => {
   const session = useSession()
+  const [isDropdown, setIsDropdown] = useState<boolean>(true)
+
+  const closeDropdown = () => {
+    setIsDropdown(false)
+  }
+
+  const openDropdown = () => {
+    setIsDropdown(true)
+  }
 
   return (
     <header className={cls.header}>
@@ -17,6 +26,7 @@ const Header = () => {
       </Link>
       <div className={cls.links}>
         <DarkModeToggle />
+
         <div className={cls.headerLinks}>
           <Link title='About' href='/about'>About</Link>
           <Link title='Portfolio' href='/portfolio'>Portfolio</Link>
@@ -39,13 +49,56 @@ const Header = () => {
               className='p-[5px] bg-[#62a21f] text-white cursor-pointer rounded-[8px] hover:bg-[#548b1a]'>
               <Image src='/log.png' alt="login" title='login' width='35' height='35' className='headerImage transform scale-x-[-1]' />
             </Link>
-            <Link
-              href='/dashboard/register'
-              className={cls.bars}>
-              <Image src='/bars.png' alt="bars" title='login' width='25' height='25' className='headerImage transform scale-x-[-1]' />
-            </Link>
           </>
         }
+        <div
+          className={cls.bars}>
+          {
+            isDropdown
+              ? <Image
+                src='/xMark.png'
+                alt="bars"
+                title='login'
+                width='22'
+                height='22'
+                className='headerImage transform scale-x-[-1]'
+                onClick={closeDropdown}
+              />
+
+              : <Image
+                src='/bars.png'
+                alt="bars"
+                title='login'
+                width='25'
+                height='25'
+                className='headerImage transform scale-x-[-1]'
+                onClick={openDropdown}
+              />
+          }
+
+          {
+            isDropdown && <div className={cls.dropdown}>
+              <div className='relative'>
+                <div className={cls.dropdownLinks}>
+                  <Link title='About' href='/about' className={cls.linkItem}>
+                    <Image width={24} height={24} src={'/about.png'} alt='...'/>
+                    <span>About</span>
+                  </Link>
+                  <div className={cls.line}></div>
+                  <Link title='Portfolio' href='/portfolio' className={cls.linkItem}>
+                    <Image width={24} height={24} src={'/portfolio.png'} alt='...'/>
+                    <span>Portfolio</span>
+                  </Link>
+                  <div className={cls.line}></div>
+                  <Link title='Posts' href='/blog' className={cls.linkItem}>
+                    <Image width={24} height={24} src={'/posts.png'} alt='...'/>
+                    <span>Posts</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
 
       </div>
 
